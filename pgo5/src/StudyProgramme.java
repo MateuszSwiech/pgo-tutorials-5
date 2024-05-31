f554af5edb2e5cf5484560ff7b84a6c79f7a99a5
MateuszSwiech <163740794+MateuszSwiech@users.noreply.github.com>
2024-05-31 20:02:40 +0200

    Update StudyProgramme.java

--- pgo5/src/StudyProgramme.java
+++ pgo5/src/StudyProgramme.java
@@ -1,16 +1,21 @@
 public class StudyProgramme {
-
     private String name;
     private String description;
     private int semester;
     private int maxITNs;
-    public StudyProgramme(String name,String description,int semester,int maxITNs){
-        this.name=name;
-        this.description=description;
-        this.semester=semester;
-        this.maxITNs=maxITNs;
 
-
+    public StudyProgramme(String name, String description, int semester, int maxITNs) {
+        this.name = name;
+        this.description = description;
+        this.semester = semester;
+        this.maxITNs = maxITNs;
     }
 
+    public int getSemester() {
+        return semester;
+    }
+
+    public int getMaxITNs() {
+        return maxITNs;
+    }
 }
