7040b31956d1c14b862a49699cedbb96d652e656
MateuszSwiech <163740794+MateuszSwiech@users.noreply.github.com>
2024-05-31 20:01:23 +0200

    Update Main.java

--- pgo5/src/Main.java
+++ pgo5/src/Main.java
@@ -1,13 +1,20 @@
 import java.util.Date;
 
 public class Main {
-    Student s=new Student("John", "Doe", "doe@wp.pl", "Warsaw, Zlota 12", "333-322-222", new Date(1980, 1, 1));
-    StudyProgramme it=new StudyProgramme("IT", "AAA", 7, 5);
+    public static void main(String[] args) {
+        Student s = new Student("John", "Doe", "doe@wp.pl", "Warsaw, Zlota 12", "333-322-222", new Date(1980, 1, 1));
+       // Student s1TEST = new Student("Jan", "Kowalski", "jan@wp.pl", "Warsaw, Zlota 44", "123-456-789", new Date(1980, 1, 1));
 
-    s.enrollStudent(it);
-    s.addGrade(5, "PGO");
-    s.addGrade(2, "APBD");
+        StudyProgramme it = new StudyProgramme("IT", "AAA", 7, 5);
 
-    Student.promoteAllStudents();
-    Student.displayInfoAboutAllStudents();
+        s.enrollStudent(it);
+     //   s1TEST.enrollStudent(it);
+
+        s.addGrade(5, "PGO");
+        s.addGrade(2, "APBD");
+
+        Student.promoteAllStudents();
+
+        Student.displayInfoAboutAllStudents();
+    }
 }
